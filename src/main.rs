34156cbfd59c68c979c3a015692b101a15d01008
rafaelgdgs <rafagsique@hotmail.com>
2025-01-13 01:28:02 -0300

    feat: OpenApi documentation

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ use axum::{
 };
 
 use serde::{Deserialize, Serialize};
-use serde_json::json;
+use serde_json::{Value, json};
 
 use sqlx::{PgPool, postgres::PgPoolOptions};
 
 use tokio::net::TcpListener;
+
+use utoipa::{OpenApi, ToSchema};
+use utoipa_swagger_ui::SwaggerUi;
 
 #[tokio::main]
 async fn main() {
@@ -36,12 +39,17 @@ async fn main() {
         .route("/", get(|| async { "Hello, World " }))
         .route("/tasks", get(get_tasks).post(create_task))
         .route("/tasks/{task_id}", patch(update_task).delete(delete_task))
+        .merge(SwaggerUi::new("/swagger-ui").url("/api-doc/openapi.json", ApiDoc::openapi()))
         .with_state(db_pool);
 
     axum::serve(listener, app)
         .await
         .expect("Error serving the application.");
 }
+
+#[derive(OpenApi)]
+#[openapi(paths(get_tasks, create_task, delete_task))]
+pub struct ApiDoc;
 
 #[derive(Serialize)]
 struct TaskRow {
@@ -50,25 +58,33 @@ struct TaskRow {
     priority: Option<i32>,
 }
 
+#[utoipa::path(
+    get,
+    path = "/tasks",
+    responses(
+    (status = 200, description = "Show all tasks"),
+    (status = 500, description = "Failed to get psql SELECT query")
+    )
+)]
 async fn get_tasks(
     State(pg_pool): State<PgPool>,
-) -> Result<(StatusCode, String), (StatusCode, String)> {
+) -> Result<(StatusCode, Json<Value>), (StatusCode, Json<Value>)> {
     let rows = sqlx::query_as!(TaskRow, "SELECT * FROM tasks ORDER BY task_id")
         .fetch_all(&pg_pool)
         .await
         .map_err(|e| {
             (
                 StatusCode::INTERNAL_SERVER_ERROR,
-                json!({ "success": false, "message": e.to_string()}).to_string(),
+                Json(json!({ "success": false, "message": e.to_string()})),
             )
         })?;
     Ok((
         StatusCode::OK,
-        json!({ "success": true, "data": rows }).to_string(),
+        Json(json!({ "success": true, "data": rows })),
     ))
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, ToSchema)]
 struct CreateTaskReq {
     name: String,
     priority: Option<i32>,
@@ -79,10 +95,18 @@ struct CreateTaskRow {
     task_id: i32,
 }
 
+#[utoipa::path(
+    post,
+    path = "/tasks",
+    responses(
+    (status = 201, description = "created user"),
+    (status = 500, description = "failed to create user")
+    )
+)]
 async fn create_task(
     State(pg_pool): State<PgPool>,
     Json(task): Json<CreateTaskReq>,
-) -> Result<(StatusCode, String), (StatusCode, String)> {
+) -> Result<(StatusCode, Json<Value>), (StatusCode, Json<Value>)> {
     let row = sqlx::query_as!(
         CreateTaskRow,
         "INSERT INTO tasks (name, priority) VALUES ($1, $2) RETURNING task_id",
@@ -94,33 +118,41 @@ async fn create_task(
     .map_err(|e| {
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            json!({ "success": false, "message": e.to_string()}).to_string(),
+            Json(json!({ "success": false, "message": e.to_string()})),
         )
     })?;
     Ok((
         StatusCode::CREATED,
-        json!({ "success": true, "data": row}).to_string(),
+        Json(json!({ "success": true, "data": row})),
     ))
 }
 
 async fn update_task(
-    State(pg_pool): State<PgPool>,
-) -> Result<(StatusCode, String), (StatusCode, String)> {
+    State(_pg_pool): State<PgPool>,
+) -> Result<(StatusCode, Json<Value>), (StatusCode, Json<Value>)> {
     todo!()
 }
 
+#[utoipa::path(
+    delete,
+    path = "/tasks/{id}",
+    responses(
+        (status = 200, description = "Deleted user with success"),
+        (status = 404, description = "Failed to find user id")
+    )
+)]
 async fn delete_task(
     State(pg_pool): State<PgPool>,
     Path(task_id): Path<i32>,
-) -> Result<(StatusCode, String), (StatusCode, String)> {
-    let row = sqlx::query!("DELETE FROM tasks WHERE task_id = $1", task_id)
+) -> Result<(StatusCode, Json<Value>), (StatusCode, Json<Value>)> {
+    let _row = sqlx::query!("DELETE FROM tasks WHERE task_id = $1", task_id)
         .execute(&pg_pool)
         .await
         .map_err(|e| {
             (
                 StatusCode::NOT_FOUND,
-                json!({ "success": false, "message": e.to_string()}).to_string(),
+                Json(json!({ "success": false, "message": e.to_string()})),
             )
         })?;
-    Ok((StatusCode::OK, json!({ "success": true}).to_string()))
+    Ok((StatusCode::OK, Json(json!({ "success": true}))))
 }
